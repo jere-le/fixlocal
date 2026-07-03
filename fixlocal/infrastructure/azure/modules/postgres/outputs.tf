66f03b29c82f870ed7c66115e414d4239204138c
Jere Lehtinen <jere@jeres-ibm-laptop.local>
2026-07-03 10:23:32 +0300

    deleted duplicates

--- fixlocal/infrastructure/azure/modules/postgres/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "fqdn"        { value = azurerm_postgresql_flexible_server.this.fqdn }
-output "server_name" { value = azurerm_postgresql_flexible_server.this.name }
-output "db_name"     { value = azurerm_postgresql_flexible_server_database.fixlocal.name }
